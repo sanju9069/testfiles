b1d5d350cb8c631e45139815998a415568e49903
Sanjay <sanj9069@gmail.com>
2019-03-17 16:35:45 +0530

    insert react file

---
+++ react-files/precache-manifest.eb78f28f54c94022a4e01f0ecdbaba1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d7c3bcceb7590136e696",
+    "url": "/static/js/main.d7c3bcce.chunk.js"
+  },
+  {
+    "revision": "eb95d78fd52838d93780",
+    "url": "/static/js/2.eb95d78f.chunk.js"
+  },
+  {
+    "revision": "d7c3bcceb7590136e696",
+    "url": "/static/css/main.09fbcf56.chunk.css"
+  },
+  {
+    "revision": "eb95d78fd52838d93780",
+    "url": "/static/css/2.03449135.chunk.css"
+  },
+  {
+    "revision": "667b4ae1d6a679e6bd75a2ec10e226c0",
+    "url": "/index.html"
+  }
+];
